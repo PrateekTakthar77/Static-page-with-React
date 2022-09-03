8c4508a8dedcc7dec4d96847b08b93800954d656
Prateek Takthar <105973557+PrateekTakthar77@users.noreply.github.com>
2022-09-03 21:35:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const task = (<div>
+    <img src="React-icon.svg.png"></img>
+    <h1>Fun Facts About React</h1>
+    
+        <p><h2>was first realised in 2013</h2></p>
+        <p><h2>was originally created by Jordan walke</h2></p>
+        <p><h2>has over 300k+ stars on github</h2></p>
+        <p><h2>is maintained by facebook</h2></p>
+        <p><h2>power thousands of enterprise app,including mobile apps</h2></p>
+    
+</div>)
+ReactDOM.render(task,document.getElementById("root"))
+
+
